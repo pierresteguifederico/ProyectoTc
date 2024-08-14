9cc97032ff6c22c259de79f4553a87aeff97190c
pierresteguifederico <144521724+pierresteguifederico@users.noreply.github.com>
2024-08-14 17:59:37 -0300

    mecq

--- src/main/java/com/fmp/proyectotc/model/Cliente.java
+++ src/main/java/com/fmp/proyectotc/model/Cliente.java
@@ -16,7 +16,7 @@ public class Cliente {
     private String apellido;
     private String dni;
 
-    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
     private List<Venta> ventas;}
 
