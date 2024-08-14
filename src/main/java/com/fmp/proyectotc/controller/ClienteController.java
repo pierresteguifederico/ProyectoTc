4b4d5e4bd650be66ca17f2da262c7a2f439a49d3
pierresteguifederico <144521724+pierresteguifederico@users.noreply.github.com>
2024-08-14 05:23:21 -0300

    cors y frontend

--- src/main/java/com/fmp/proyectotc/controller/ClienteController.java
+++ src/main/java/com/fmp/proyectotc/controller/ClienteController.java
@@ -2,6 +2,8 @@ package com.fmp.proyectotc.controller;
 
 import com.fmp.proyectotc.model.Cliente;
 import com.fmp.proyectotc.service.ClienteService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -20,19 +22,44 @@ public class ClienteController {
     }
 
     @PostMapping("/crear")
-    public Cliente createCliente(@RequestBody Cliente cliente) {
-        return clienteService.saveCliente(cliente);
+    public ResponseEntity<Object> crearCliente (@RequestBody Cliente cliente) {
+        try{
+            clienteService.saveCliente(cliente);
+            return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+        } catch (Exception e){
+            return new ResponseEntity<>("Error interno del servidor",HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }}
+
+    @GetMapping ("/id/{id}")
+    public ResponseEntity<Cliente> obtenerCliente(@PathVariable Long id_cliente) {
+        Cliente cliente = clienteService.getClienteById(id_cliente);
+        if (cliente != null) {
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
-    @GetMapping ("/{id}")
-    public Cliente obtenerCliente(@PathVariable Long id){
-        return clienteService.getClienteById(id);}
-
-    @DeleteMapping ("{id}")
+    @DeleteMapping ("/eliminar/{id_cliente}")
     public void deleteCliente(@PathVariable Long id){
         clienteService.deleteCliente(id);
 
     }
-
+    @PutMapping("/editar/{codigo_producto}")
+    public ResponseEntity<Object> modificarProducto(@PathVariable Long id_cliente, @RequestBody Cliente cliente) {
+        try {
+            Cliente clienteExistente = clienteService.getClienteById(id_cliente);
+            if (clienteExistente == null) {
+                return new ResponseEntity<>("Producto no encontrado", HttpStatus.NOT_FOUND);
+            }
+            cliente.setId_cliente(id_cliente);
+            clienteService.saveCliente(cliente);
+            return new ResponseEntity<>(cliente, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Error interno del servidor", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
+
+
+
